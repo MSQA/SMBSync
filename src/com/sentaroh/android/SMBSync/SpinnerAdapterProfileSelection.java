59126b653dacf808108391f3f88043a6f0ba6584
Sen-home <Sen-home@SEN-HOME>
2014-06-30 13:46:13 +0900

    同期プロファイルのマスター/ターゲットスピナーICONを変更

--- src/com/sentaroh/android/SMBSync/SpinnerAdapterProfileSelection.java
+++ src/com/sentaroh/android/SMBSync/SpinnerAdapterProfileSelection.java
@@ -25,7 +25,15 @@ OTHER DEALINGS IN THE SOFTWARE.
 
 import static com.sentaroh.android.SMBSync.Constants.SMBSYNC_PROF_TYPE_REMOTE;
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
+import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
@@ -40,6 +48,8 @@ public class SpinnerAdapterProfileSelection extends ArrayAdapter<String> {
 	private int mTextColor=0;
 	@SuppressWarnings("unused")
 	private int mTextSize=0;
+	
+	private Drawable mRemoteDrawable=null, mMobileDrawable=null;
 
 	public void setTextColor(int color) {mTextColor=color;}
 	public void setTextSize(int size_sp) {mTextSize=size_sp;}
@@ -48,8 +58,50 @@ public class SpinnerAdapterProfileSelection extends ArrayAdapter<String> {
 		super(c, textViewResourceId);
 //		mResourceId=textViewResourceId;
 		mContext=c;
+		
+    	Bitmap bitmap_r = BitmapFactory.decodeResource(mContext.getResources(), R.drawable.ic_32_server);
+    	Bitmap bitmap_m = BitmapFactory.decodeResource(mContext.getResources(), R.drawable.ic_32_mobile);
+    	Bitmap bitmap_a = BitmapFactory.decodeResource(mContext.getResources(), android.R.drawable.arrow_down_float);
+    	
+    	int width_r = bitmap_r.getWidth();
+    	int height_r = bitmap_r.getHeight();
+    	int width_m = bitmap_m.getWidth();
+    	int height_m = bitmap_m.getHeight();
+    	int width_a = bitmap_a.getWidth(); 
+//    	int height_a = bitmap_a.getHeight(); 
+    	int spacer=(int) toPixel(mContext.getResources(),5);
+//    	Log.v("","sp="+spacer);
+    	Bitmap rem_bmap = Bitmap.createBitmap(width_r+spacer+width_a, height_r, Bitmap.Config.ARGB_8888);
+    	Canvas rem_canvas = new Canvas(rem_bmap);
+    	
+    	rem_canvas.drawBitmap(bitmap_r, 0, 0, (Paint)null);
+//    	int dis_width_r = width_r + bitmap_a.getWidth();
+    	int dis_height_r = (height_r - bitmap_a.getHeight()) / 2;
+    	rem_canvas.drawBitmap(bitmap_a, width_r+spacer, dis_height_r, (Paint)null); 
+
+    	Bitmap mob_bmap = Bitmap.createBitmap(width_m+spacer+width_a, height_m, Bitmap.Config.ARGB_8888);
+    	Canvas mob_canvas = new Canvas(mob_bmap);
+    	mob_canvas.drawBitmap(bitmap_m, 0, 0, (Paint)null);
+//    	int dis_width_m = width_m + bitmap_a.getWidth();
+    	int dis_height_m = (height_m - bitmap_a.getHeight()) / 2;
+    	mob_canvas.drawBitmap(bitmap_a, width_m+spacer, dis_height_m, (Paint)null); 
+
+    	mRemoteDrawable=new BitmapDrawable(mContext.getResources(),rem_bmap);
+
+    	mMobileDrawable=new BitmapDrawable(mContext.getResources(),mob_bmap);
+    	
+//    	rem_bmap.recycle();
+//    	mob_bmap.recycle();
+    	bitmap_a.recycle();
+    	bitmap_r.recycle();
+    	bitmap_m.recycle();
 	}
 	
+	final static private float toPixel(Resources res, int dip) {
+		float px = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dip, res.getDisplayMetrics());
+		return px;
+	};
+
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
         TextView view;
@@ -66,15 +118,17 @@ public class SpinnerAdapterProfileSelection extends ArrayAdapter<String> {
         view.setGravity(Gravity.CENTER_VERTICAL);
         view.setCompoundDrawablePadding(10);
         if (type.equals(SMBSYNC_PROF_TYPE_REMOTE)) {
-            view.setCompoundDrawablesWithIntrinsicBounds(mContext.getResources().getDrawable(android.R.drawable.arrow_down_float),
-            		null,
-            		mContext.getResources().getDrawable(R.drawable.ic_32_server), 
-            		null);
+            view.setCompoundDrawablesWithIntrinsicBounds(mRemoteDrawable,null,null,null);
+//            view.setCompoundDrawablesWithIntrinsicBounds(mContext.getResources().getDrawable(android.R.drawable.arrow_down_float),
+//            		null,
+//            		mContext.getResources().getDrawable(R.drawable.ic_32_server), 
+//            		null);
         } else {
-            view.setCompoundDrawablesWithIntrinsicBounds(mContext.getResources().getDrawable(android.R.drawable.arrow_down_float),
-            		null,
-            		mContext.getResources().getDrawable(R.drawable.ic_32_mobile), 
-            		null);
+        	view.setCompoundDrawablesWithIntrinsicBounds(mMobileDrawable,null,null,null);
+//            view.setCompoundDrawablesWithIntrinsicBounds(mContext.getResources().getDrawable(android.R.drawable.arrow_down_float),
+//            		null,
+//            		mContext.getResources().getDrawable(R.drawable.ic_32_mobile), 
+//            		null);
         }
 
 //        if (text_color!=0) view.setTextColor(text_color);
